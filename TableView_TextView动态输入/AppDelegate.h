b4232cff40f00807c501a3abb0fd71950b877ec3
leixiang <541150547@qq.com>
2017-05-11 11:39:23 +0800

    Initial Commit

---
+++ TableView_TextView动态输入/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView_TextView动态输入
+//
+//  Created by 雷祥 on 2017/5/11.
+//  Copyright © 2017年 okdeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableView_TextView动态输入/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableView_TextView动态输入
+//
+//  Created by 雷祥 on 2017/5/11.
+//  Copyright © 2017年 okdeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
